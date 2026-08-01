884bdf5e77369aa697189790284b2302c090655f
Denis <zalizkodenis12@gmail.com>
2026-08-01 20:16:00 +0300

    Adjust intersection observer rootMargin for visible fade-out

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@
           }
         }
       });
-    }, { threshold: 0.1, rootMargin: '0px 0px 0px 0px' });
+    }, { threshold: 0, rootMargin: '-80px 0px -80px 0px' });
 
     revealTargets.forEach((el) => observer.observe(el));
   }
